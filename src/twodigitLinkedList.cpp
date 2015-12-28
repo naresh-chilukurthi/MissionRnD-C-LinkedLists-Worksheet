c2bb51a0515c3160dbf1840057b6ce8c43dd6614
naresh117 <nareshchilukurthi@gmail.com>
2015-12-28 17:46:35 +0530

    Submit Commit

--- src/twodigitLinkedList.cpp
+++ src/twodigitLinkedList.cpp
@@ -22,5 +22,20 @@ struct node {
 };
 
 int convert_sll_2digit_to_int(struct node *head){
-	return 0;
+	struct node* temp;int sum=0,num;
+	temp=head;
+	while(temp)
+    {
+        num=(temp->digit1)*10+(temp->digit2);
+        if(num>9)
+        {
+            sum=sum*100+num;
+        }
+        else if(num>0&& num<=9)
+            {
+                sum=sum*10+num;
+            }
+        temp=temp->next;
+    }
+    return sum;
 }
